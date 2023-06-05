8405d2aa86e8c246f8a11c5e9797f7e857bd7b90
sousa10x <80914528+sousa10x@users.noreply.github.com>
2023-06-05 13:45:15 -0300

    projeto postado

---
+++ 3_scriptConsulta.js
@@ -0,0 +1,108 @@
+
+let Modal = document.querySelector("dialog")
+let FecharBotao = document.getElementById("BotaoFechar")
+
+FecharBotao.addEventListener('click', function () {
+    Modal.close()
+    document.getElementById("informativo").innerHTML = "";
+    document.getElementById("InputDialog").value = "";
+});
+var Tr1Pagar = document.getElementById("Tr1Pagar")
+Tr1Pagar.addEventListener('click', () => {
+    Modal.showModal()
+
+    Valor = document.getElementById('Tr1Valor').innerHTML;
+    Tipo = document.getElementById('Tr1Tipo').innerHTML;
+    // Modalidade = document.getElementById('Tr1Modalidade').innerHTML;
+
+    document.getElementById("valorModal").innerHTML = Valor;
+    document.getElementById("tipoModal").innerHTML = Tipo;
+    // document.getElementById("modalidadeModal").innerHTML = Modalidade;
+
+});
+var Tr2Pagar = document.getElementById("Tr2Pagar");
+Tr2Pagar.addEventListener('click', () => {
+    Valor = document.getElementById('Tr2Valor').innerHTML;
+    Tipo = document.getElementById('Tr2Tipo').innerHTML;
+    // Modalidade = document.getElementById('Tr2Modalidade').innerHTML;
+
+    document.getElementById("valorModal").innerHTML = Valor;
+    document.getElementById("tipoModal").innerHTML = Tipo;
+    // document.getElementById("modalidadeModal").innerHTML = Modalidade;
+    Modal.showModal()
+
+});
+var Tr3Pagar = document.getElementById("Tr3Pagar");
+Tr3Pagar.addEventListener('click', () => {
+
+    Valor = document.getElementById('Tr3Valor').innerHTML
+    Tipo = document.getElementById('Tr3Tipo').innerHTML
+    // Modalidade = document.getElementById('Tr3Modalidade').innerHTML
+
+    document.getElementById("valorModal").innerHTML = Valor
+    document.getElementById("tipoModal").innerHTML = Tipo
+    // document.getElementById("modalidadeModal").innerHTML = Modalidade
+    Modal.showModal()
+});
+var Tr4Pagar = document.getElementById("Tr4Pagar");
+Tr4Pagar.addEventListener('click', () => {
+
+    Valor = document.getElementById('Tr4Valor').innerHTML;
+    Tipo = document.getElementById('Tr4Tipo').innerHTML;
+    // Modalidade = document.getElementById('Tr4Modalidade').innerHTML;
+
+    document.getElementById("valorModal").innerHTML = Valor;
+    document.getElementById("tipoModal").innerHTML = Tipo;
+    // document.getElementById("modalidadeModal").innerHTML = Modalidade;
+    Modal.showModal()
+
+});
+function ChaveValorModal() {
+    localStorage.setItem('KeyTrValor', Valor);
+    localStorage.setItem('KeyTrTipo', Tipo);
+    // localStorage.setItem('KeyTrModalidade', Modalidade);
+}
+
+function enviar() {
+    BotaoEnviar = location.href = "5_Pagamento.html"
+}
+
+////////// trabalhando o envio do modal //////
+let EnviarModal = document.getElementById("enviarModal");
+EnviarModal.onclick = function () {
+    let InputDialog = document.getElementById("InputDialog").value;
+    if (InputDialog == "") {
+
+        var informarCPF = "🚫 PREENCHA O CAMPO COM O CPF DO CLIENTE🚫"
+        informarCPF = informarCPF.fontcolor('#FFFFF')
+        document.getElementById("informativo").innerHTML = informarCPF;
+    } else {
+
+        enviar();
+        document.getElementById("InputDialog").value = ""
+        Modal.close();
+        ChaveValorModal()
+    }
+
+}
+// let ModalDataId = document.getElementById("ModalDataId")
+// let BotaoAbrirModalData = document.getElementById("BotaoAbrirModalData")
+// BotaoAbrirModalData.addEventListener('click', function () {
+//     ModalDataId.showModal()
+// })
+
+// let FecharModalData = document.getElementById("FecharModalData")
+// FecharModalData.addEventListener('click', function () {
+//     ModalDataId.close()
+// })
+
+
+
+
+
+// $('verFucao').on('click', function() {
+//     var valores = document.querySelectorAll("table tr td");
+//     for (i = 0; i < valores.length; i++) {
+//         console.log(valores[i].innerHTML);
+//     }
+// })
---
+++ 5-scriptPagamento.js
@@ -0,0 +1,8 @@
+
+// window.addEventListener("load", function () {
+// })
+document.getElementById("recebePagamento").innerHTML = localStorage.getItem('KeyTrValor')
+document.getElementById("RecebeServico").innerHTML = localStorage.getItem('KeyTrTipo')
+
+
+
